ff96c6a8f8831caa42f23c1a375883b6b9605d73
Svetlana Smirnova <apelsinka2002sveta@gmail.com>
2023-03-09 21:27:32 +0300

    Initial commit

---
+++ src/js/script.js
@@ -0,0 +1,67 @@
+const isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+        return(
+            isMobile.Android() ||
+            isMobile.BlackBerry() ||
+            isMobile.iOS() ||
+            isMobile.Opera() ||
+            isMobile.Windows()
+        );
+    }
+};
+
+if (isMobile.any()){
+    document.body.classList.add('__touch');
+}else{
+    document.body.classList.add('__pc');
+}
+
+
+// Меню
+const menu__icon = document.querySelector('.menu__icon');
+if(menu__icon){
+    const menu__body = document.querySelector('.menu__body');
+    menu__icon.addEventListener("click", function(e){
+        document.body.classList.toggle('__lock');
+        menu__icon.classList.toggle('__active');
+        menu__body.classList.toggle('__active');
+    })
+}
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
+}
